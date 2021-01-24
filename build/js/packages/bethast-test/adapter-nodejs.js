145315c39bf764a6eaa61e2ddb83a0141094b0d0
doompickaxe <kwatzal@gmail.com>
2021-01-24 10:29:00 +0100

    Add gitignore

--- build/js/packages/bethast-test/adapter-nodejs.js
+++
@@ -1,2 +0,0 @@
-require("/home/kev/IdeaProjects/bethast/build/js/node_modules/kotlin-test-js-runner/kotlin-test-nodejs-runner.js")
-module.exports = require("/home/kev/IdeaProjects/bethast/build/js/packages/bethast-test/kotlin/bethast-test.js")
